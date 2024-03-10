9568a9d20b0d884d8e901e4c8416c7cb23c9156e
Goose <107567328+SomeRandomGoose@users.noreply.github.com>
2024-03-10 14:58:57 +0100

    jamikaz sfx

--- Assets/Jam/Dammage/EnemyTrigger.cs
+++ Assets/Jam/Dammage/EnemyTrigger.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class EnemyTrigger : MonoBehaviour,IDamageable
 {
     public AEnemy owner;
-
+    public AudioSource damageSound;
 
     public void Damage(float amount)
     {
         owner.healthController.Dammage(amount);
+        damageSound.Play();
     }
 
     public void OnTriggerEnter(Collider other)
